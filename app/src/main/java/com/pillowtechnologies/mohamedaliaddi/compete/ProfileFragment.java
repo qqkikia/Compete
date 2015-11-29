c7faad6480cd5b9ec9ad3652e26752be22a5e230
Mohamed Ali Addi <qqkikia@gmail.com>
2015-11-29 20:49:44 +0100

    New Stuff Added with regards to the calendar

--- app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/ProfileFragment.java
+++ app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/ProfileFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
---
+++ app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/User.java
@@ -0,0 +1,20 @@
+package com.pillowtechnologies.mohamedaliaddi.compete;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by mohamedaliaddi on 29/11/15.
+ */
+public class User {
+    String naam;
+    Bitmap profilepic;
+
+
+
+
+    public void User(String n, Bitmap p){
+        naam = n;
+        profilepic = p;
+    }
+
+}
