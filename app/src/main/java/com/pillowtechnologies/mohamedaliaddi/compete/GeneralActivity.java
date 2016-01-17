6d54f9ad47522fff8854c310d214a8aaeed394f7
iman <i.hashemi@students.uu.nl>
2016-01-17 14:50:42 +0100

    Login

--- app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/GeneralActivity.java
+++ app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/GeneralActivity.java
@@ -1,41 +1,27 @@
 package com.pillowtechnologies.mohamedaliaddi.compete;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
-import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
-import com.facebook.login.widget.ProfilePictureView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -68,10 +54,7 @@ public class GeneralActivity extends AppCompatActivity {
         Resources res =getResources();
         adapter=new CustomAdapter( CustomListView, currevents,res);
         list.setAdapter(adapter);
-        ProfilePictureView pf = (ProfilePictureView) findViewById(R.id.profilepic);
 
-
-        profile = Profile.getCurrentProfile();
 
 
         new GraphRequest(
