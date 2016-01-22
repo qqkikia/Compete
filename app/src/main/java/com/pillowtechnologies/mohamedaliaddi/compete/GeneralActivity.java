2425f72ae79ddb506fbb3673e732b209cf53f7de
qqkikia <qqkikia@gmail.com>
2016-01-22 12:06:50 +0100

    Update GeneralActivity.java

--- app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/GeneralActivity.java
+++ app/src/main/java/com/pillowtechnologies/mohamedaliaddi/compete/GeneralActivity.java
@@ -1,6 +1,7 @@
 package com.pillowtechnologies.mohamedaliaddi.compete;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -10,7 +11,11 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -42,6 +47,12 @@ import com.facebook.login.widget.ProfilePictureView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -49,20 +60,29 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class GeneralActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    String[] drawerlist;
+
     DrawerLayout drawerLayout;
-    ListView listView;
-    Drawable d;
-    Profile profile;
+    int x = 500;
     ListView list;
     CustomAdapter adapter;
     GeneralActivity CustomListView = null;
-    ArrayList<Event> currevents = new ArrayList<Event>();
+
     GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
+    Location location;
+    List<ParseObject> objectListing;
+    List<ParseObject> objectListing1;
+    List<ParseObject> objectListing2;
+    ArrayList<ParseObject> potentialmatches;
+    String matchee ="";
+    ArrayList<ParseObject> currentmatches;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +93,8 @@ public class GeneralActivity extends AppCompatActivity implements
         CustomListView = this;
         setListData();
         Resources res = getResources();
-        adapter = new CustomAdapter(CustomListView, currevents, res);
+
+
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -81,11 +102,47 @@ public class GeneralActivity extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+        currentmatches = new ArrayList<ParseObject>();
+        potentialmatches = new ArrayList<ParseObject>();
+
+        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, locationListener);
+        location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            getEvents();
+        getCurrentMatches();
+        adapter = new CustomAdapter(CustomListView, currentmatches, res);
+
+        list.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
 
 
     }
 
-    @Override
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_general, menu);
@@ -140,8 +197,7 @@ public class GeneralActivity extends AppCompatActivity implements
     public void onConnected(Bundle bundle) {
 
 
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
+
 
     }
 
@@ -190,6 +246,11 @@ public class GeneralActivity extends AppCompatActivity implements
     public void toPlan(View view) {
         Intent intent = new Intent(this, PlanActivity.class);
         startActivity(intent);
+        if(x != 500){
+        potentialmatches = parseLocation();
+        }
+        String y = String.valueOf(x);
+        Toast.makeText(this,y,Toast.LENGTH_SHORT).show();
     }
 
     public void toLadder(View view) {
@@ -198,34 +259,36 @@ public class GeneralActivity extends AppCompatActivity implements
     }
 
     public void toCurrent(View view) {
-        Intent intent = new Intent(this, CurrentActivity.class);
+        /*Intent intent = new Intent(this, CurrentActivity.class);
+        startActivity(intent);*/
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        query.getInBackground(potentialmatches.get(0).getObjectId().toString(), new GetCallback<ParseObject>() {
+            public void done(ParseObject user2, ParseException e) {
+                if (e == null) {
+                    matchee = (String) user2.get("User1");
+                    user2.put("User2", ParseUser.getCurrentUser().getUsername().toString());
+                    user2.saveInBackground();
+                }
+            }
+        });
+        Toast.makeText(this,"You have been matched with " + matchee,Toast.LENGTH_SHORT).show();
+
+
+
+
+    }
+
+
+
+    public void toUserlist(View view){
+        Intent intent = new Intent(this,UserList.class);
         startActivity(intent);
     }
 
-    public void hamburgerslide(View view) {
-
-        drawerLayout.openDrawer(Gravity.LEFT);
-
-    }
-
-
     public void setListData()
     {
-        Event e1 = new Event();
-        e1.setTitle("Event 1");
-        Event e2 = new Event();
-        e2.setTitle("Event 2");
-        Event e3 = new Event();
-        e3.setTitle("Event 3");
-        Event e4 = new Event();
-        e4.setTitle("Event 4");
-        Event e5 = new Event();
-        e5.setTitle("Event 5");
-        currevents.add(e1);
-        currevents.add(e2);
-        currevents.add(e3);
-        currevents.add(e4);
-        currevents.add(e5);
+
 
 
 
@@ -241,5 +304,158 @@ public class GeneralActivity extends AppCompatActivity implements
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
+    }
+
+    public void getAvailableMatches(){
+
+    }
+
+    public ArrayList<ParseObject> parseLocation(){
+ArrayList<ParseObject> list = new ArrayList<ParseObject>();
+        if(objectListing.size() != 0) {
+        for(int i = 0; i < objectListing.size(); i++) {
+            Location mlocation = new Location("");
+
+            String latstring = (String) objectListing.get(i).get("Latitude");
+            String lonstring = (String) objectListing.get(i).get("Longitude");
+            double lat = Double.parseDouble(latstring);
+            double lon = Double.parseDouble(lonstring);
+            mlocation.setLatitude(lat);
+            mlocation.setLongitude(lon);
+            float distance = mlocation.distanceTo(location);
+            int roundeddistance = Math.round(distance);
+            if (roundeddistance <= 5000) {
+
+                list.add(objectListing.get(i));
+            }
+        }
+        }
+        return list;
+    }
+
+
+    public void SportNow(View view){
+        if(x != 500){
+            potentialmatches = parseLocation();
+        }
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        if(potentialmatches.size() != 0) {
+            query.getInBackground(potentialmatches.get(0).getObjectId().toString(), new GetCallback<ParseObject>() {
+                public void done(ParseObject user2, ParseException e) {
+                    if (e == null) {
+                        matchee = (String) user2.get("User1");
+                        user2.put("User2", ParseUser.getCurrentUser().getUsername().toString());
+                        user2.saveInBackground();
+                        right();
+                        getCurrentMatches();
+                    }
+
+                }
+            });
+        }else{Wrong();}
+    }
+
+    public void right(){
+        Toast.makeText(this,"You have been matched with " + matchee, Toast.LENGTH_SHORT).show();
+    }
+    public void Wrong(){
+        Toast.makeText(this,"Something went wrong, please try later", Toast.LENGTH_SHORT).show();
+    }
+    public void getEvents(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        query.whereNotEqualTo("User1", ParseUser.getCurrentUser().getUsername().toString());
+        query.whereEqualTo("User2", "empty");
+        query.whereNotEqualTo("EventStatus", "Closed");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objectList, ParseException e) {
+                if (e == null) {
+                    x = objectList.size();
+                    objectListing = objectList;
+
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+        String y = String.valueOf(x);
+        Toast.makeText(this,y,Toast.LENGTH_SHORT).show();
+    }
+
+    public void getCurrentMatches(){
+        ParseQuery<ParseObject> query1 = ParseQuery.getQuery("Events");
+        query1.whereEqualTo("User1", ParseUser.getCurrentUser().getUsername().toString());
+        query1.whereNotEqualTo("User2", "empty");
+        query1.whereNotEqualTo("EventStatus", "Closed");
+        query1.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objectList, ParseException e) {
+                if (e == null) {
+
+                    objectListing1 = objectList;
+
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+        ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Events");
+        query2.whereEqualTo("User2", ParseUser.getCurrentUser().getUsername().toString());
+        query2.whereNotEqualTo("User1", "empty");
+        query2.whereNotEqualTo("EventStatus", "Closed");
+        query2.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objectList, ParseException e) {
+                if (e == null) {
+
+                    objectListing2 = objectList;
+
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                createCurrentMatchesList();
+
+            }
+        }, 2000);
+    }
+
+    public void createCurrentMatchesList(){
+        currentmatches.clear();
+        if(objectListing1.size() != 0) {
+            for (int i = 0; i < objectListing1.size(); i++) {
+        if(!(currentmatches.size() == 0)) {
+                for (int x = 0; x < currentmatches.size(); x++) {
+
+        if (!(currentmatches.get(x).getObjectId().equals(objectListing1.get(i).getObjectId()))) {
+            if(!(currentmatches.contains(objectListing1.get(i)))) {
+                currentmatches.add(objectListing1.get(i));
+            }
+        }
+    }
+}else{currentmatches.add(objectListing1.get(i));}
+
+            }
+        }
+
+        if(objectListing2.size() != 0) {
+            for (int i = 0; i < objectListing2.size(); i++) {
+                if(!(currentmatches.size() == 0)) {
+                    for (int x = 0; x < currentmatches.size(); x++) {
+
+                        if (!(currentmatches.get(x).getObjectId().equals(objectListing2.get(i).getObjectId()))) {
+                            if(!(currentmatches.contains(objectListing2.get(i)))) {
+                                currentmatches.add(objectListing2.get(i));
+                            }
+                        }
+                    }
+                }else{currentmatches.add(objectListing2.get(i));}
+
+            }
+        }
+
+        Toast.makeText(this,String.valueOf(currentmatches.size()),Toast.LENGTH_SHORT).show();
+        adapter.notifyDataSetChanged();
     }
 }
